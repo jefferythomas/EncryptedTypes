0f36c28124551ca1a1c346eab5d19912133f4c19
Jeffery Thomas <jeffery.thomas@gmail.com>
2019-04-11 09:05:11 -0400

    Split the unit tests into public and internal tests

---
+++ EncryptedTypesTests/EncryptedTypesInternalTests.swift
@@ -0,0 +1,47 @@
+//
+//  EncryptedTypesInternalTests.swift
+//  EncryptedTypesTests
+//
+//  Created by Thomas, Jeffery on 4/11/19.
+//  Copyright © 2019 JLT Source. All rights reserved.
+//
+
+import XCTest
+@testable import EncryptedTypes
+
+class EncryptedTypesInternalTests: XCTestCase {
+
+    func testSymmetric() {
+        let symmetric = Symmetric()
+        let text = "This is my test.  Will it work?"
+
+        let encrypted = symmetric.encrypt(Data(text.utf8))
+        let decrypted = symmetric.decrypt(encrypted)
+
+        XCTAssertNotEqual(encrypted, Data(text.utf8))
+        XCTAssertEqual(String(bytes: decrypted, encoding: .utf8), text)
+    }
+
+    func testSymmetricInitKeyIv() {
+        let testKey = "This is my test key."
+        let testIv = "This is my test iv."
+
+        let symmetric = Symmetric(key: Data(testKey.utf8), iv: Data(testIv.utf8))
+
+        XCTAssertEqual(String(bytes: symmetric.key, encoding: .utf8), testKey)
+        XCTAssertEqual(String(bytes: symmetric.iv, encoding: .utf8), testIv)
+    }
+
+    func testSymmetricReuseability() {
+        let symmetric1 = Symmetric()
+        let symmetric2 = Symmetric(key: symmetric1.key, iv: symmetric1.iv)
+        let text = "This is my test.  Will it work?"
+
+        let encrypted = symmetric1.encrypt(Data(text.utf8))
+        let decrypted = symmetric2.decrypt(encrypted)
+
+        XCTAssertNotEqual(encrypted, Data(text.utf8))
+        XCTAssertEqual(String(bytes: decrypted, encoding: .utf8), text)
+    }
+
+}
--- EncryptedTypesTests/EncryptedTypesTests.swift
+++ EncryptedTypesTests/EncryptedTypesTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import EncryptedTypes
+import EncryptedTypes
 
 class EncryptedTypesTests: XCTestCase {
 
@@ -69,39 +69,6 @@ class EncryptedTypesTests: XCTestCase {
 
         encryptedString.value = nil
         XCTAssertEqual(encryptedString.value, nil)
-    }
-
-    func testSymmetric() {
-        let symmetric = Symmetric()
-        let text = "This is my test.  Will it work?"
-
-        let encrypted = symmetric.encrypt(Data(text.utf8))
-        let decrypted = symmetric.decrypt(encrypted)
-
-        XCTAssertNotEqual(encrypted, Data(text.utf8))
-        XCTAssertEqual(String(bytes: decrypted, encoding: .utf8), text)
-    }
-
-    func testSymmetricInitKeyIv() {
-        let testKey = "This is my test key."
-        let testIv = "This is my test iv."
-
-        let symmetric = Symmetric(key: Data(testKey.utf8), iv: Data(testIv.utf8))
-
-        XCTAssertEqual(String(bytes: symmetric.key, encoding: .utf8), testKey)
-        XCTAssertEqual(String(bytes: symmetric.iv, encoding: .utf8), testIv)
-    }
-
-    func testSymmetricReuseability() {
-        let symmetric1 = Symmetric()
-        let symmetric2 = Symmetric(key: symmetric1.key, iv: symmetric1.iv)
-        let text = "This is my test.  Will it work?"
-
-        let encrypted = symmetric1.encrypt(Data(text.utf8))
-        let decrypted = symmetric2.decrypt(encrypted)
-
-        XCTAssertNotEqual(encrypted, Data(text.utf8))
-        XCTAssertEqual(String(bytes: decrypted, encoding: .utf8), text)
     }
 
 }
